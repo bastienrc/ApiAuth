e5ae52d0f6fb6e8ca081650620a9760e9ea90c90
Sebastien Colbert <colbertsebastien@gmail.com>
2021-11-03 08:02:27 +0100

    Add app base

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    const decodedToken = jwt.verify(token, process.env.AUTH_TOKEN)
+    const userId = decodedToken.userId
+    if (req.body.userId && req.body.userId !== userId) {
+      throw new Error('User ID non valable')
+    } else {
+      next()
+    }
+  } catch (error) {
+    res.status(401).json({ error: error | 'Requête non authentifiée !' })
+  }
+}
