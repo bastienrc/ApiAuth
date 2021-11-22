9a1c636da598a5640a2c1ab336706876294b5644
Sebastien Colbert <colbertsebastien@gmail.com>
2021-11-22 22:06:51 +0100

    Add elt in userSchema + Security

--- models/user.js
+++ models/user.js
@@ -1,7 +1,25 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
+const Schema = mongoose.Schema
 
-const userSchema = mongoose.Schema({
+const userSchema = new Schema({
+  userId: mongoose.ObjectId,
+  firstname: {
+    type: String,
+    trim: true,
+    minLength: [2, 'Trop petit, au moins 2 caractères'],
+    maxLength: [42, 'Oh ! du calme pas plus de 42 à la fois, je vous prie.']
+  },
+  lastname: {
+    type: String,
+    trim: true,
+    minLength: [2, 'C\'est pas beaucoup, allez au moins 2 lettres'],
+    maxLength: [42, 'Oh ! Bah, ca fait beaucoup ! 42 grand max.']
+  },
+  bio: {
+    type: String,
+    maxLength: [300, 'Eh ! Pas besoin de racontez votre vie, juste vous présentez.']
+  },
   email: {
     type: String,
     lowercase: true,
@@ -24,8 +42,19 @@ const userSchema = mongoose.Schema({
   }
 },
 {
-  timestamps: true
+  runValidators: true
 })
+
+mongoose.plugin(schema => {
+  schema.pre('findOneAndUpdate', setRunValidators)
+  schema.pre('updateMany', setRunValidators)
+  schema.pre('updateOne', setRunValidators)
+  schema.pre('update', setRunValidators)
+})
+
+function setRunValidators () {
+  this.setOptions({ runValidators: true })
+}
 
 userSchema.plugin(uniqueValidator)
 
